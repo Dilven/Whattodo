372c79c9794b49b48f1afc6c27f0423cfb57ba79
Dilven <mateusiakkrystian@gmail.com>
2019-06-10 10:25:56 +0200

    Shortening path to google auth

--- src/helpers/googleAuth.ts
+++ src/helpers/googleAuth.ts
@@ -1,11 +1,21 @@
 import { paths } from '../configs/paths.config';
 import qs from 'qs';
 
-export const authLink = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=${
-  paths.domain
-}/${paths.client.auth}&response_type=token&client_id=${
-  process.env.REACT_APP_GOOGLE_CLIENT_ID
-}`;
+const options = {
+  scope: 'https://www.googleapis.com/auth/userinfo.email',
+  include_granted_scopes: true,
+  state: 'state_parameter_passthrough_value',
+  redirect_uri: `${paths.domain}/${paths.client.auth}`,
+  response_type: 'token',
+  client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID
+};
+
+const params = Object.entries(options).reduce(
+  (acc, [key, value]) => acc + `&${key}=${value}`,
+  ''
+);
+
+export const authLink = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
 
 export const getAccessTokenFromLocation = (location: { hash: string }) => {
   const { hash } = location;
